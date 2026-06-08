ab3902f4180495079ddc23854aafd2f380d2cf2e
Jay Yadav <yadavjay081105@gmail.com>
2026-06-08 10:36:50 +0530

    build marker test

--- customer_app/lib/presentation/screens/permission_screen.dart
+++ customer_app/lib/presentation/screens/permission_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:geolocator/geolocator.dart';
 import '../../application/providers/permission_provider.dart';
 import '../../application/providers/location_provider.dart';
 
